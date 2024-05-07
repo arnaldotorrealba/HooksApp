8ac08c99a37db33f7b3e9150febac49b212bcc94
arnaldotorr <arnaldo.torrealba@gmail.com>
2024-05-06 20:14:03 -0400

    First Commit

---
+++ src/01-useState/CounterCustomHook.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const CounterCustomHook = () => {
+  return (
+    <>
+      <h1>Counter with hook: XXX</h1>
+      <hr />
+
+      <button>+1</button>
+    </>
+  )
+}
